5d11cca5aa9320c4bfe93bf3e1af01bc44398d02
sxtnflur <135318909+sxtnflur@users.noreply.github.com>
2024-05-21 21:13:30 +0300

    dsa

--- webRTCserver/webRTCserver.py
+++ webRTCserver/webRTCserver.py
@@ -13,7 +13,7 @@ cors = CORS(app, origins=['*'], methods=['GET', 'POST', 'PUT', 'PATCH', 'DELETE'
 connected_particpants = {}
 
 def write_log(s):    
-    with open('logfile.out', 'a+') as f:
+    with open('webrtc/webRTCserver/logfile.txt', 'a+') as f:
         f.write('time: %s Action: %s \n' % (str(datetime.datetime.now()), s))
 
 @app.route('/')
@@ -23,6 +23,7 @@ def index():
 
 @sio.on('message', namespace='/')
 def messgage(sid, data):
+    write_log('message')
     sio.emit('message', data=data)
 
 @sio.on('disconnect', namespace='/')
@@ -35,26 +36,38 @@ def disconnect(sid):
         except ValueError:
             write_log("Remove %s from %s \n list of left participants is %s has failed" %(sid, room, clients))
 
-@sio.on('create or join', namespace='/')
+@sio.on('create_or_join', namespace='/')
 def create_or_join(sid, data):
-    sio.enter_room(sid, data)
+    # sio.emit('created', data)
+    print('create or join')
+    print(sid)
+    print(data)
+    room = data.get('room')
+    user_id = data.get('user_id')
+    print(room)
+    print(user_id)
+    sio.enter_room(sid, room)
+    print(connected_particpants)
+
     try:
-        connected_particpants[data].append(sid)
+        connected_particpants[room].append(sid)
     except KeyError:
-        connected_particpants[data] = [sid]
-    numClients = len(connected_particpants[data])
+        connected_particpants[room] = [sid]
+
+    numClients = len(connected_particpants[room])
+
     if numClients == 1:
-        sio.emit('created', data)
+        sio.emit('created', room)
     elif numClients > 2:
         sio.emit('full')
-    elif numClients == 2:
+    else:
         sio.emit('joined')
         sio.emit('join')
-    print (sid, data, len(connected_particpants[data]))
+    print(sid, room, len(connected_particpants[room]))
 
-@app.route('/<room>')
-def room(room):
-    return render_template('index.html', room=room)
+@app.route('/<room>/<user_id>')
+def room(room, user_id):
+    return render_template('index.html', room=room, user_id=user_id)
 
 if __name__ == '__main__':
     app = socketio.Middleware(sio, app)
