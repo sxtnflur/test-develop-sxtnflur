ee5800d67fb179b80c95f99ce32ead6e94587b98
sxtnflur <135318909+sxtnflur@users.noreply.github.com>
2024-05-21 10:13:18 +0300

    das

--- stream/sockets.py
+++ stream/sockets.py
@@ -1,9 +1,11 @@
 import base64
 import os
+import random
+
 import cv2
 import numpy as np
-from flask import Flask, render_template, send_from_directory
-from flask_socketio import SocketIO, emit, join_room, leave_room
+from flask import Flask, render_template, send_from_directory, redirect
+from flask_socketio import SocketIO, emit, join_room, leave_room, rooms
 from flask_cors import CORS
 from utils import send_error_msg
 from flask import session
@@ -15,6 +17,8 @@ cors = CORS(app, origins=['*'], methods=['GET', 'POST', 'PUT', 'PATCH', 'DELETE'
 socketio = SocketIO(app, cors_allowed_origins='*')
 
 
+is_room = False
+
 def base64_to_image(base64_string):
     base64_data = base64_string.split(",")[1]
     image_bytes = base64.b64decode(base64_data)
@@ -23,11 +27,8 @@ def base64_to_image(base64_string):
     return image
 
 @socketio.on("connect")
-def handle_connect():
+def connect():
     print("Connected")
-    emit("my response", {
-       "data": "Connected"
-    })
 
 @socketio.on("disconnect")
 def handle_disconnect():
@@ -38,6 +39,8 @@ def handle_disconnect():
 
 @socketio.on("image")
 def receive_image(image):
+    # print("IMAGE")
+    # print(image)
     image = base64_to_image(image)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     frame_resized = cv2.resize(gray, (640, 360))
@@ -46,18 +49,49 @@ def receive_image(image):
     processed_img_data = base64.b64encode(frame_encoded).decode()
     b64_src = "data:image/jpg;base64,"
     processed_img_data = b64_src + processed_img_data
-    emit("processed_image", processed_img_data)
+    emit("processed_image", processed_img_data, broadcast=True)
 
-@app.route("/send")
-def send():
-   return render_template("send.html")
+@socketio.on('joined')
+def joined(data):
+    # print("joined")
+    """Sent by clients when they enter a room.
+    A status message is broadcast to all people in the room."""
+    room = data.get('room')
+    is_translator = data.get("is_translator")
+    # print(room)
+    # print(is_translator)
+    # if not is_room:
+    #     is_room = True
+    # else:
+    # if room == 7:
+    join_room(room)
+
+    emit('join', {'message': 'test'}, room=room)
+
+@socketio.on('leave')
+def leave(data):
+    user_id = data.get("user_id")
+    room = data.get('room')
+    leave_room(room)
+    emit('leave', {
+        "user_id": user_id
+    })
 
 
-@app.route("/receive")
-def receive():
-   return render_template("receive.html")
+# @app.route("/send")
+# def send():
+#    return render_template("send.html")
+#
+#
+@app.route("/trans/<int:room>/<int:is_translator>")
+def receive(room, is_translator):
+   return render_template("index.html", room=room, is_translator=is_translator)
 
 
+@app.route('/send')
+def index():
+    user_id = random.randint(1, 10)
+    return redirect(f"/trans/{user_id}/1")
 
 if __name__ == "__main__":
    socketio.run(app, debug = True, port = 5000, host = '0.0.0.0')
--- stream/templates/static/app.js
+++ stream/templates/static/app.js
@@ -2,37 +2,76 @@
 document.addEventListener("DOMContentLoaded", function() {
     const socket = io.connect({ autoConnect: false });
 
-    if (navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true })
-            .then((stream) => {
-                const video = document.querySelector("#videoElement");
-                video.srcObject = stream;
-                video.play();
-            })
-            .catch((error) => {
-                console.error("Error accessing camera:", error);
-            });
+    const is_translator = document.getElementsByTagName("video")[0].id;
+
+    if (is_translator == 1) {
+        if (navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia({ video: true })
+                .then((stream) => {
+    //                const video = document.querySelector("#videoElement");
+                    const video = document.getElementsByTagName("video")[0];
+//                    console.log(video)
+                    video.srcObject = stream;
+                    video.play();
+                })
+                .catch((error) => {
+                    console.error("Error accessing camera:", error);
+                });
+        }
     }
 
     const canvas = document.getElementById("canvas");
     const context = canvas.getContext("2d");
+//    const room = document.title
 
-    const FPS = 10;
-    setInterval(() => {
-        const video = document.querySelector("#videoElement");
-        const width = canvas.width;
-        const height = canvas.height;
-        context.drawImage(video, 0, 0, width, height);
-        const data = canvas.toDataURL("image/jpeg", 0.5);
-        context.clearRect(0, 0, width, height);
-        socket.emit("image", data);
-    }, 1000 / FPS);
+    socket.on("connect", function(value){
+//        console.log("Connected")
+//        console.log(value)
+        socket.emit("joined", {room: document.title, is_translator: is_translator})
+    });
 
+    socket.on("join", function(value) {
+//        console.log("joined")
+//        console.log(value)
+    });
+
+    socket.on("leave", function(value) {
+//        console.log("leaved")
+//        console.log(value)
+    });
+
+    if (is_translator == 1) {
+        const FPS = 30;
+        setInterval(() => {
+            const video = document.getElementsByTagName("video")[0];
+            const width = canvas.width;
+            const height = canvas.height;
+            context.drawImage(video, 0, 0, width, height);
+            const data = canvas.toDataURL("image/jpeg", 0.5);
+            context.clearRect(0, 0, width, height);
+            socket.emit("image", data);
+            }
+
+
+        , 1000 / FPS);
+    }
+    console.log(is_translator)
+
+    if (is_translator == 0) {
     socket.on("processed_image", (image) => {
-        console.log("processed_image");
-        console.log(image);
-        const photo = document.getElementById("photo");
-        photo.setAttribute("src", image);
+//        if (is_translator == 0) {
+//            console.log("processed_image");
+//            console.log(image);
+            const photo = document.getElementById("photo");
+//            console.log("processed_image2");
+//            console.log(image);
+            photo.setAttribute("src", image);
+
+        });
+    }
+
+    socket.on('status', function(data) {
+        console.log(data);
     });
 
     socket.connect();
